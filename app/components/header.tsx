2fb54d1c7579861e551c16702164ff234f657993
Shakhram <shakag6@gmail.com>
2024-09-03 14:41:04 -0400

    Wrap Link around elements so entire elemnt is clickable, for easier use

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,16 +4,22 @@ export default function Header() {
     return (
         <header className="text-white p-4 md:p-6" >
             <nav className="flex justify-between items-center">
-                <h1 className="text-2xl md:text-3xl font-bold">
-                    <Link href="/">FitBeats AI</Link>
-                </h1>
+                <Link href="/">
+                    <h1 className="text-2xl md:text-3xl font-bold">
+                        FitBeats AI
+                    </h1>
+                </Link>
                 <div className="space-x-4">
-                    <button className="bg-white text-purple-600 px-4 py-2 rounded-full font-semibold hover:bg-opacity-90 transition duration-300">
-                        <Link href="/sign-up">Sign Up</Link>
-                    </button>
-                    <button className="bg-transparent border border-white px-4 py-2 rounded-full font-semibold hover:bg-white hover:text-purple-600 transition duration-300">
-                        <Link href="/log-in">Log In</Link>
-                    </button>
+                    <Link href="/sign-up">
+                        <button className="bg-white text-purple-600 px-4 py-2 rounded-full font-semibold hover:bg-opacity-90 transition duration-300">
+                            Sign Up
+                        </button>
+                    </Link>
+                    <Link href="/log-in">
+                        <button className="bg-transparent border border-white px-4 py-2 rounded-full font-semibold hover:bg-white hover:text-purple-600 transition duration-300">
+                            Log In
+                        </button>
+                    </Link>
                 </div>
             </nav>
         </header>
