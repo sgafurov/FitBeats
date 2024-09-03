ae4c2acfb2142039f34f350f7f8bc454d8636169
Shakhram <shakag6@gmail.com>
2024-09-03 14:48:36 -0400

    Remove alternate platform signup/login options

--- app/log-in/page.tsx
+++ app/log-in/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     return (
         <div className="min-h-screen justify-center bg-gradient-to-r from-blue-500 to-purple-600">
             <Header />
-            <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 flex items-center justify-center p-4 text-black ">
+            <div className="min-h-screen flex items-center justify-center p-4 text-black ">
                 {/* <div className="bg-white p-8 rounded-lg shadow-md w-96"> */}
                 <div className="bg-white p-8 rounded-lg w-96">
                     <h1 className="text-3xl font-bold mb-6 text-center text-gray-800 font-roboto">
@@ -43,19 +43,7 @@ export default function Page() {
                             Sign Up
                         </a>
                     </div>
-                    <div className="mt-6">
-                        <p className="text-center text-sm text-gray-600 mb-4">
-                            Or login with
-                        </p>
-                        <div className="flex justify-center space-x-4">
-                            <button className="flex items-center justify-center w-12 h-12 rounded-full bg-red-500 text-white hover:bg-red-600 transition duration-300">
-                                <i className="fab fa-google"></i>
-                            </button>
-                            <button className="flex items-center justify-center w-12 h-12 rounded-full bg-blue-600 text-white hover:bg-blue-700 transition duration-300">
-                                <i className="fab fa-facebook-f"></i>
-                            </button>
-                        </div>
-                    </div>
+                   
                     <p className="mt-8 text-center text-xs text-gray-500">
                         Need help? Contact support@aiworkoutmusic.com
                     </p>
